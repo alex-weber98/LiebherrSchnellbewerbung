3257f38e83a9bb670c5978fff69440e16741af81
alex-weber98 <65651039+alex-weber98@users.noreply.github.com>
2023-11-15 15:00:30 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,43 @@
 
+// function sendMail(body)
+// {
+//     var transporter = nodemailer.createTransport(smtpTransport({
+//         host: "smtpanon.liebherr.i", // hostname
+//         secure: false, // use SSL
+//         port: 25, // port for secure SMTP
+//         tls: {
+//            rejectUnauthorized: false
+//         }
+//     }));
+
+//  var mailOptions = {
+//         from: 'client.lbh@liebherr.com', // sender address
+//         to: 'alexander.weber@liebherr.com', // list of receivers
+//         cc: '', // Comma separated list or an array
+//         subject: 'Schnellbewerbung', // Subject line
+//         html: body // html body
+//     };
+
+// transporter.sendMail(mailOptions, function(error, info){
+//         if(error){
+//             console.log("/sendmail error");
+//             console.log(error);
+//             res.sendStatus(500);
+//             return;
+//         }else{
+//             console.log("Message sent: " + info.response);
+//             // if you don't want to use this transport object anymore, uncomment following line
+//             socketTimeout: 30 * 1000 // 0.5 min: Time of inactivity until the connection is closed
+//             transporter.close(); // shut down the connection pool, no more messages
+//             res.sendStatus(200);
+//         }
+
+//         // if you don't want to use this transport object anymore, uncomment following line
+//         transporter.close(); // shut down the connection pool, no more messages
+//     });
+// }
+
+
 function createCheckbox(parentElement, cbxName){
     element.innerHTML += `
     <input type="checkbox" id="cbx_${cbxName}" name="${cbxName}" value="${cbxName}">
@@ -6,6 +45,36 @@ function createCheckbox(parentElement, cbxName){
     `;    
 }
 
+function sendMail(body){
+    const credentials = {
+        host: 'smtpanon.liebherr.i',
+        port: 25,
+        secure: false,
+        // auth: {
+        //   user: 'username',
+        //   pass: 'password'    
+        // }
+      }
+      smtp.connect(credentials)
+        .then(info => {
+          // connected, credentials OK 
+        })
+
+    const email = {
+        from: 'client.lbh@liebherr.com',
+        to: 'alexander.weber@liebherr.com',
+        subject: 'Schnellbewerbung',
+        html: body
+    }
+
+    smtp.addMailToQueue(email);
+
+    // connect and send entire queue
+    smtp.connect(credentials)
+        .then(() => smtp.flushQueue())
+        .catch(err => console.error(err));
+
+}
 
 
 // console.log("Hello world!");
@@ -43,6 +112,22 @@ for(let i = 0; i < cbxBereich.length; i++)
 {
     let item = cbxBereich[i];
     createCheckbox(element, item);
+}
+
+
+function save(filename, data) {
+    const blob = new Blob([data], {type: 'text/csv'});
+    if(window.navigator.msSaveOrOpenBlob) {
+        window.navigator.msSaveBlob(blob, filename);
+    }
+    else{
+        const elem = window.document.createElement('a');
+        elem.href = window.URL.createObjectURL(blob);
+        elem.download = filename;        
+        document.body.appendChild(elem);
+        elem.click();        
+        document.body.removeChild(elem);
+    }
 }
 
 
@@ -129,25 +214,47 @@ function submit(){
     
     `;
 
+    // sendMail(body);
 
-    Email.send({
-        Host : "smtp.elasticemail.com",
-        Username : "liebherr@bewerbungsformular.com",
-        Password : "95AB8BD9262CBAE0C0C7419E84B757E8130D",
-        // To : 'alex_-_weber@hotmail.com',
-        To : "alexander.weber@liebherr.com",
-        From : "alex_-_weber@hotmail.com",
-        Subject : "Schnellbewerbung",
-        Body : body
-    }).then(
-      message => alert(message)
-    );
+    
+    // Email.send({
+    //     Host : "smtpanon.liebherr.i",
+    //     To : "alexander.weber@liebherr.com",
+    //     From : "client.lbh@liebherr.com",
+    //     Username: "",
+    //     Password: "",
+    //     Subject : "Schnellbewerbung",
+    //     Body : body
+    // }).then(
+    //   message => alert(message)
+    // );
 
-    location.reload();
+    // Email.send({
+    //     Host : "smtp.elasticemail.com",
+    //     Username : "liebherr@bewerbungsformular.com",
+    //     Password : "95AB8BD9262CBAE0C0C7419E84B757E8130D",
+    //     // To : 'alex_-_weber@hotmail.com',
+    //     To : "alexander.weber@liebherr.com",
+    //     From : "alex_-_weber@hotmail.com",
+    //     Subject : "Schnellbewerbung",
+    //     Body : body
+    // }).then(
+    //   message => alert(message)
+    // );
+
+    // var blob = new Blob(["Welcome to Websparrow.org."],
+    //     { type: "text/plain;charset=utf-8" });
+    
+    // saveAs(blob, "static.txt");
+
+    save("test.txt", body);
+    // location.reload();
 
 }
 
-function sendEmail() { 
+
+
+// function sendEmail() { 
     // Email.send({ 
     //   Host: "smtpanon.liebherr.i", 
     //   //Username: "client.lbh@liebherr.com",
@@ -177,4 +284,4 @@ function sendEmail() {
     //     }); 
 
 
-} 
+// } 
