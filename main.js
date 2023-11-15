38b2984e310164fb5032a554ff39c796772ee57d
alex-weber98 <65651039+alex-weber98@users.noreply.github.com>
2023-11-15 15:25:40 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,43 +1,4 @@
 
-// function sendMail(body)
-// {
-//     var transporter = nodemailer.createTransport(smtpTransport({
-//         host: "smtpanon.liebherr.i", // hostname
-//         secure: false, // use SSL
-//         port: 25, // port for secure SMTP
-//         tls: {
-//            rejectUnauthorized: false
-//         }
-//     }));
-
-//  var mailOptions = {
-//         from: 'client.lbh@liebherr.com', // sender address
-//         to: 'alexander.weber@liebherr.com', // list of receivers
-//         cc: '', // Comma separated list or an array
-//         subject: 'Schnellbewerbung', // Subject line
-//         html: body // html body
-//     };
-
-// transporter.sendMail(mailOptions, function(error, info){
-//         if(error){
-//             console.log("/sendmail error");
-//             console.log(error);
-//             res.sendStatus(500);
-//             return;
-//         }else{
-//             console.log("Message sent: " + info.response);
-//             // if you don't want to use this transport object anymore, uncomment following line
-//             socketTimeout: 30 * 1000 // 0.5 min: Time of inactivity until the connection is closed
-//             transporter.close(); // shut down the connection pool, no more messages
-//             res.sendStatus(200);
-//         }
-
-//         // if you don't want to use this transport object anymore, uncomment following line
-//         transporter.close(); // shut down the connection pool, no more messages
-//     });
-// }
-
-
 function createCheckbox(parentElement, cbxName){
     element.innerHTML += `
     <input type="checkbox" id="cbx_${cbxName}" name="${cbxName}" value="${cbxName}">
@@ -45,45 +6,7 @@ function createCheckbox(parentElement, cbxName){
     `;    
 }
 
-function sendMail(body){
-    const credentials = {
-        host: 'smtpanon.liebherr.i',
-        port: 25,
-        secure: false,
-        // auth: {
-        //   user: 'username',
-        //   pass: 'password'    
-        // }
-      }
-      smtp.connect(credentials)
-        .then(info => {
-          // connected, credentials OK 
-        })
 
-    const email = {
-        from: 'client.lbh@liebherr.com',
-        to: 'alexander.weber@liebherr.com',
-        subject: 'Schnellbewerbung',
-        html: body
-    }
-
-    smtp.addMailToQueue(email);
-
-    // connect and send entire queue
-    smtp.connect(credentials)
-        .then(() => smtp.flushQueue())
-        .catch(err => console.error(err));
-
-}
-
-
-// console.log("Hello world!");
-
-
-
-// var x = document.createElement("INPUT");
-// x.setAttribute("type", "checkbox");
-// element.appendChild(x);
 
 const cbxBereich = ["Administration",
 "Buchhaltung / Finanzen /Controlling",
@@ -133,14 +56,6 @@ function save(filename, data) {
 
 function submit(){
 
-    // let doc = document.;
-
-    // window.location.href = "mailto:?subject=" + document.title + "&body=" + doc;
-
-    // window.open('mailto:alexander.weber@liebherr.com?subject=subject&body=body');
-    // window.location.href = "mailto:?subject=" + document.title + "&body=" + encodeURI(document.location);
-
-
     let bereiche = [];
     let erfahrung;
 
@@ -174,17 +89,6 @@ function submit(){
         }
     }
 
-    // console.log(erfahrung);
-
-
-    //let body = "";
-
-    // let body = document.getElementsByTagName("body")
-    // body = JSON.stringify(body);
-    
-    
-    // let body = document.body.outerHTML;
-    // console.log(body);
 
     console.log(bereiche);
 
@@ -192,96 +96,29 @@ function submit(){
 
     for(let i = 0; i < bereiche.length; i++)
     {
-        bereicheLi += `<li> ${bereiche[i]}</li>`;
+        bereicheLi += `- ${bereiche[i]}\r\n`;
     }
 
     console.log(bereicheLi);
 
 
     body=`
-        <p>Bereiche:</p>
-        <ul>
-            ${bereicheLi}
-        </ul>
+Bereiche:
+${bereicheLi}
 
-        <p>Berufserfahrung: ${erfahrung}</p>
+Berufserfahrung: ${erfahrung}
 
-        <p>Vorname: ${document.getElementById("Vorname").value}</p>
-        <p>Nachname: ${document.getElementById("Nachname").value}</p>
-        <p>Telefonnummer: ${document.getElementById("Telefonnummer").value}</p>
-        <p>Email: ${document.getElementById("E-Mail").value}</p>
-        <p>LinkedIn: ${document.getElementById("LinkedIn").value}</p>
-    
-    `;
+Vorname: ${document.getElementById("Vorname").value}
+Nachname: ${document.getElementById("Nachname").value}
+Telefonnummer: ${document.getElementById("Telefonnummer").value}
+Email: ${document.getElementById("E-Mail").value}
+LinkedIn: ${document.getElementById("LinkedIn").value}
+`;
 
-    // sendMail(body);
+ 
+    save(`${document.getElementById("Vorname").value}_${document.getElementById("Nachname").value}.txt`, body);
 
-    
-    // Email.send({
-    //     Host : "smtpanon.liebherr.i",
-    //     To : "alexander.weber@liebherr.com",
-    //     From : "client.lbh@liebherr.com",
-    //     Username: "",
-    //     Password: "",
-    //     Subject : "Schnellbewerbung",
-    //     Body : body
-    // }).then(
-    //   message => alert(message)
-    // );
-
-    // Email.send({
-    //     Host : "smtp.elasticemail.com",
-    //     Username : "liebherr@bewerbungsformular.com",
-    //     Password : "95AB8BD9262CBAE0C0C7419E84B757E8130D",
-    //     // To : 'alex_-_weber@hotmail.com',
-    //     To : "alexander.weber@liebherr.com",
-    //     From : "alex_-_weber@hotmail.com",
-    //     Subject : "Schnellbewerbung",
-    //     Body : body
-    // }).then(
-    //   message => alert(message)
-    // );
-
-    // var blob = new Blob(["Welcome to Websparrow.org."],
-    //     { type: "text/plain;charset=utf-8" });
-    
-    // saveAs(blob, "static.txt");
-
-    save("test.txt", body);
-    // location.reload();
+    location.reload();
 
 }
 
-
-
-// function sendEmail() { 
-    // Email.send({ 
-    //   Host: "smtpanon.liebherr.i", 
-    //   //Username: "client.lbh@liebherr.com",
-        
-    //   //From: "client.lbh@liebherr.com", 
-    //   To: 'alexander.weber@liebherr.com', 
-    //   Subject: "Sending Email using javascript", 
-    //   Body: "Test", 
-    // }) 
-    //   .then(function (message) { 
-    //     alert("mail sent successfully") 
-    //   }); 
-
-
-    //   Email.send({ 
-    //     Host: "smtp.elasticemail.com", 
-    //     Username: "liebherr@bewerbungsformular.com",
-    //     Passwort: "95AB8BD9262CBAE0C0C7419E84B757E8130D",
-          
-    //     From: "liebherr@bewerbungsformular.com", 
-    //     To: 'alexander.weber@liebherr.com', 
-    //     Subject: "Sending Email using javascript", 
-    //     Body: "Test", 
-    //   }) 
-    //     .then(function (message) { 
-    //       alert("mail sent successfully") 
-    //     }); 
-
-
-// } 
