baaea1e5ee87707aac49a991e901c165ca95199a
alex-weber98 <65651039+alex-weber98@users.noreply.github.com>
2023-11-20 12:57:04 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -27,8 +27,13 @@ const cbxBereich = ["Administration",
 "Sicherheitswesen",
 "Vertrieb"];
 
+const cbxBewerbungfür =["Praktikum",
+"Bachelor-/Master-/Diplomarbeit",
+"Direkteinstieg",
+"Trainee"]
 
-var cbxName = "Administration";
+
+// var cbxName = "Administration";
 var element = document.getElementById("bereich");
 
 for(let i = 0; i < cbxBereich.length; i++)
@@ -38,8 +43,15 @@ for(let i = 0; i < cbxBereich.length; i++)
 }
 
 
+var element = document.getElementById("bewerbungfür");
+for(let i = 0; i< cbxBewerbungfür.length; i++){
+    let item = cbxBereich[i];
+    createCheckbox(element, item);
+}
+
+
 function save(filename, data) {
-    const blob = new Blob([data], {type: 'text/csv'});
+    const blob = new Blob([data], {type: 'plain/text'});
     if(window.navigator.msSaveOrOpenBlob) {
         window.navigator.msSaveBlob(blob, filename);
     }
@@ -61,14 +73,30 @@ function delay(time) {
 function submit(){
 
     let bereiche = [];
+    let bewerbungfür = [];
     let erfahrung;
 
     console.log("i was clicked")
 
+    // ----------- Bewerbung für ------------------
+    var element = document.getElementById("bewerbungfür");
+
+    var children = element.children;
+
+    for(let i = 0; i < children.length; i++)
+    {
+        let item = children[i];
+        if(item.type == "checkbox" && item.checked)
+        {
+            console.log(item.value);
+            bewerbungfür.push(item.value);
+        }
+    }
+
     // ----------- Bereiche ------------------
     var element = document.getElementById("bereich");
 
-    let children = element.children;
+    var children = element.children;
 
     for(let i = 0; i < children.length; i++)
     {
@@ -96,7 +124,7 @@ function submit(){
 
     console.log(bereiche);
 
-    bereicheLi = "";
+    var bereicheLi = "";
 
     for(let i = 0; i < bereiche.length; i++)
     {
@@ -105,12 +133,28 @@ function submit(){
 
     console.log(bereicheLi);
 
+    var beewerbungFürLi = "";
 
-    body=`
-Bereiche:
+    for(let i = 0; i < bewerbungfür.length; i++)
+    {
+        beewerbungFürLi += `- ${bewerbungfür[i]}\r\n`;
+    }
+
+    console.log(beewerbungFürLi);
+
+
+    var body=`
+Bewerbung für:
+${beewerbungFürLi}
+
+Bereiche als:
 ${bereicheLi}
 
 Berufserfahrung: ${erfahrung}
+
+Abgeschlossene Ausbildung: ${document.getElementById("AusbildungAbgeschlossen").value}
+
+Aktuelle Ausbildung: ${document.getElementById("AusbildungAktuell").value}
 
 Vorname: ${document.getElementById("Vorname").value}
 Nachname: ${document.getElementById("Nachname").value}
